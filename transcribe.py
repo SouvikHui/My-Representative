33e2380d77f6523190920988b23079cb6f9e1e0f
Souvik Hui <155455536+SouvikHui@users.noreply.github.com>
2025-06-18 11:21:47 +0530

    Add files via upload

---
+++ transcribe.py
@@ -0,0 +1,20 @@
+# whisper transcription
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def transcribe_audio(audio_path: str):
+    with open(audio_path, "rb") as file:
+        transcription = client.audio.transcriptions.create(
+            file=file,
+            model="whisper-large-v3-turbo",
+            response_format="verbose_json",
+            timestamp_granularities=["segment"],
+            language="en",
+            temperature=0.0
+        )
+    return transcription.text
